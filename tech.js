bdc5efba1be823b4449173b6aefeece857f05bd3
hp <asadhussainshad@gmail.com>
2024-04-11 04:09:17 +0500

    first commit

---
+++ tech.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgWhite.red(`\n\tWELCOME TO WORD COUNTER APP`)); // start greetings
+let userWords = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: chalk.bgBlue.white("\nPlease type you paragraph here:\n")
+    }]);
+// write a program for cointing words
+let words = userWords.sentence.trim().split(" ");
+// write a program for counting characters
+let characters = userWords.sentence.replace(/\s/g, "");
+// print above write programs
+console.log(chalk.bgWhite.green(`\nYou have typed ${words.length} wrods in this paragraph`)); // shows the court
+console.log(chalk.bgWhite.blue(`\nYou have typed ${characters.length} characters in this paragraph`)); // shows the character count
+console.log(chalk.bgRed.white(`\n\tTHANKYOU FOR TYPING PARAGRAPH HERE`)); // end greetings
